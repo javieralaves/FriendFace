da3c4e83083c7d3087265736b1398ad9b73807f4
Javier Alaves <jayalaves@gmail.com>
2023-07-28 16:45:15 +0200

    Core Data subclasses

---
+++ FriendFace/CachedFriend+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CachedFriend+CoreDataClass.swift
+//  FriendFace
+//
+//  Created by Javier Alaves on 28/7/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CachedFriend)
+public class CachedFriend: NSManagedObject {
+
+}
---
+++ FriendFace/CachedUser+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CachedUser+CoreDataClass.swift
+//  FriendFace
+//
+//  Created by Javier Alaves on 28/7/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CachedUser)
+public class CachedUser: NSManagedObject {
+
+}
